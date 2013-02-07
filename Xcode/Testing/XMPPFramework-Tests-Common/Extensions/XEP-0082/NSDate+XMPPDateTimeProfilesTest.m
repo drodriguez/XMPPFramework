1ce0aef4fa8d7e602cd4c7b10d14665b4bbc6434
Daniel Rodríguez Troitiño <drodrigueztroitino@yahoo.es>
2013-02-07 01:31:31 +0100

    Test copied from old testing project. All green.
    The test should be computer independent (by being careful with time zones and
    stuff).

--- Xcode/Testing/XMPPFramework-Tests-Common/Extensions/XEP-0082/NSDate+XMPPDateTimeProfilesTest.m
+++ Xcode/Testing/XMPPFramework-Tests-Common/Extensions/XEP-0082/NSDate+XMPPDateTimeProfilesTest.m
@@ -1,5 +1,5 @@
 //
-//  XMPPDateTimeProfilesTest.m
+//  NSDate+XMPPDateTimeProfilesTest
 //  XMPPFramework-Tests-Mac
 //
 //  Created by Daniel Rodríguez Troitiño on 06/02/13.
@@ -16,22 +16,14 @@
 
 #import "XMPPFramework.h"
 
-@interface XMPPDateTimeProfilesTest : GHTestCase
+@interface NSDateXMPPDateTimeProfilesTest : GHTestCase
 
 @end
 
-@implementation XMPPDateTimeProfilesTest
+@implementation NSDateXMPPDateTimeProfilesTest
 {
     NSDateFormatter *_dateFormatter;
     NSTimeZone *_originalTimeZone;
-}
-
-- (void)setUpClass
-{
-    _dateFormatter = [[NSDateFormatter alloc] init];
-    [_dateFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
-    [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS z"];
-    [_dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
 }
 
 - (void)setUp
@@ -39,7 +31,11 @@
     // Since dates do not have a explicit time zone, they are parsed in the
     // current timezone. To make this test repeatable we must fix the time zone.
     _originalTimeZone = [NSTimeZone defaultTimeZone];
-    [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneWithName:@"US/Pacific"]];
+
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    [_dateFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS z"];
 }
 
 - (void)tearDown
@@ -47,171 +43,75 @@
     [NSTimeZone setDefaultTimeZone:_originalTimeZone];
 }
 
-+ (void)testDateWithXmppDateStringShouldSucceed
-{
-    #define XMPPAssertParseDateEqualStrings(date, expected, description, ...) \
-        GHAssertEqualStrings([_dateFormatter stringFromDate:[NSDate dateWithXmppDateString:(date)]], expected, description, ##__VA_ARGS__);
+// Since the constructors just call the underlaying XMPPDateTimeProfiles methods
+// we just do a simple smoke test.
 
-    XMPPAssertParseDateEqualStrings(@"1776-07-04", @"1776-07-04 00:00:00.000 GMT-00:14:44", nil);
-    XMPPAssertParseDateEqualStrings(@"1969-01-21", @"1969-01-21 00:00:00.000 GMT+01:00", nil);
-    XMPPAssertParseDateEqualStrings(@"1969-07-21", @"1969-07-21 00:00:00.000 GMT+01:00", nil);
-	XMPPAssertParseDateEqualStrings(@"2010-04-04", @"2010-04-04 00:00:00.000 GMT+02:00", nil);
-	XMPPAssertParseDateEqualStrings(@"2010-12-25", @"2010-12-25 00:00:00.000 GMT+01:00", nil);
+- (void)testDateConstructors
+{
+    GHAssertEqualStrings([_dateFormatter stringFromDate:[NSDate dateWithXmppDateString:@"1776-07-04"]], @"1776-07-04 00:00:00.000 GMT-07:52:58", nil);
+    GHAssertNil([NSDate dateWithXmppDateString:@"1776-7-4"], nil);
+
+    // The date part will be the current day
+    NSDateFormatter *dayFormatter = [[NSDateFormatter alloc] init];
+    [dayFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [dayFormatter setDateFormat:@"yyyy-MM-dd"];
+
+    // The tz part will be the one of the current day (DST/Standard)
+    NSDateFormatter *tzFormatter = [[NSDateFormatter alloc] init];
+    [tzFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [tzFormatter setDateFormat:@"z"];
+
+    NSString *expected = [NSString stringWithFormat:@"%@ %@ %@",
+                          [dayFormatter stringFromDate:[NSDate date]],
+                          @"14:00:00.123",
+                          [tzFormatter stringFromDate:[NSDate date]]];
+    GHAssertEqualStrings([_dateFormatter stringFromDate:[NSDate dateWithXmppTimeString:@"16:00:00.123-06:00"]], expected, nil);
+    GHAssertNil([NSDate dateWithXmppTimeString:@"16:00:00-0600"], nil);
+
+    GHAssertEqualStrings([_dateFormatter stringFromDate:[NSDate dateWithXmppDateTimeString:@"2010-04-04T21:56:15.123-05:00"]], @"2010-04-04 19:56:15.123 GMT-07:00", nil);
+    GHAssertNil([NSDate dateWithXmppDateTimeString:@"1969-7-4T02:56:15.123Z"], nil);
 }
 
-+ (void)testDateWithXmppDateStringShouldFail
+- (void)testXmppDateString
 {
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.calendar = [NSCalendar currentCalendar];
+    dateComponents.year = 1776;
+    dateComponents.month = 7;
+    dateComponents.day = 4;
+    dateComponents.timeZone = [[NSTimeZone alloc] initWithName:@"US/Eastern"];
+
+    GHAssertEqualStrings([[dateComponents date] xmppDateString], @"1776-07-04", nil);
 }
 
-+ (void)testDateWithXmppTimeString
+- (void)testXmppTimeString
 {
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.calendar = [NSCalendar currentCalendar];
+    dateComponents.year = 2013;
+    dateComponents.month = 2;
+    dateComponents.day = 7;
+    dateComponents.hour = 12;
+    dateComponents.minute = 34;
+    dateComponents.second = 56;
+    dateComponents.timeZone = [[NSTimeZone alloc] initWithName:@"US/Eastern"];
+
+    GHAssertEqualStrings([[dateComponents date] xmppTimeString], @"17:34:56Z", nil);
 }
 
-+ (void)testDateWithXmppTimeString
+- (void)testXmppDateTimeString
 {
-}
+    NSDateComponents *dateComponents = [[NSDateComponents alloc] init];
+    dateComponents.calendar = [NSCalendar currentCalendar];
+    dateComponents.year = 2013;
+    dateComponents.month = 2;
+    dateComponents.day = 7;
+    dateComponents.hour = 12;
+    dateComponents.minute = 34;
+    dateComponents.second = 56;
+    dateComponents.timeZone = [[NSTimeZone alloc] initWithName:@"US/Eastern"];
 
-+ (void)testXmppDateString
-{
-}
-
-+ (void)testXmppTimeString
-{
-}
-
-+ (void)testXmppDateTimeString
-{
+    GHAssertEqualStrings([[dateComponents date] xmppDateTimeString], @"2013-02-07T17:34:56Z", nil);
 }
 
 @end
-
-/*
-+ (void)testParseTime
-{
-	// Should Succeed
-	//
-	// Notice the proper time zones in the output.
-	// All have been converted to the local time zone as needed.
-
-	NSString *t1s = @"16:00:00";
-	NSString *t2s = @"16:00:00Z";
-	NSString *t3s = @"16:00:00-06:00";
-	NSString *t4s = @"16:00:00.123";
-	NSString *t5s = @"16:00:00.123Z";
-	NSString *t6s = @"16:00:00.123-06:00";
-
-	NSLog(@"S parseTime(%@) = %@", t1s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t1s]]);
-	NSLog(@"S parseTime(%@) = %@", t2s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t2s]]);
-	NSLog(@"S parseTime(%@) = %@", t3s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t3s]]);
-	NSLog(@"S parseTime(%@) = %@", t4s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t4s]]);
-	NSLog(@"S parseTime(%@) = %@", t5s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t5s]]);
-	NSLog(@"S parseTime(%@) = %@", t6s, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t6s]]);
-
-	NSLog(@" ");
-
-	// Should Fail
-
-	NSString *t1f = nil;
-	NSString *t2f = @"16-00-00";
-	NSString *t3f = @"16:00:00-0600";
-	NSString *t4f = @"16:00:00.1";
-	NSString *t5f = @"16:00.123";
-	NSString *t6f = @"cheese";
-
-	NSLog(@"F parseTime(%@) = %@", t1f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t1f]]);
-	NSLog(@"F parseTime(%@) = %@", t2f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t2f]]);
-	NSLog(@"F parseTime(%@) = %@", t3f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t3f]]);
-	NSLog(@"F parseTime(%@) = %@", t4f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t4f]]);
-	NSLog(@"F parseTime(%@) = %@", t5f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t5f]]);
-	NSLog(@"F parseTime(%@) = %@", t6f, [df stringFromDate:[XMPPDateTimeProfiles parseTime:t6f]]);
-}
-
-+ (void)testParseDateTime
-{
-	// Should Succeed
-	//
-	// Notice the proper time zones in the output.
-
-	NSString *dt01s = @"1776-07-04T02:56:15Z";
-	NSString *dt02s = @"1776-07-04T21:56:15-05:00";
-	NSString *dt03s = @"1776-07-04T02:56:15.123Z";
-	NSString *dt04s = @"1776-07-04T21:56:15.123-05:00";
-
-	NSLog(@"S parseDateTime(%@) = %@", dt01s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt01s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt02s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt02s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt03s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt03s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt04s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt04s]]);
-
-	NSLog(@" ");
-
-	NSString *dt05s = @"1969-01-21T02:56:15Z";
-	NSString *dt06s = @"1969-01-20T21:56:15-05:00";
-	NSString *dt07s = @"1969-01-21T02:56:15.123Z";
-	NSString *dt08s = @"1969-01-21T21:56:15.123-05:00";
-
-	NSLog(@"S parseDateTime(%@) = %@", dt05s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt05s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt06s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt06s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt07s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt07s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt08s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt08s]]);
-
-	NSLog(@" ");
-
-	NSString *dt09s = @"1969-07-21T02:56:15Z";
-	NSString *dt10s = @"1969-07-20T21:56:15-05:00";
-	NSString *dt11s = @"1969-07-21T02:56:15.123Z";
-	NSString *dt12s = @"1969-07-21T21:56:15.123-05:00";
-
-	NSLog(@"S parseDateTime(%@) = %@", dt09s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt09s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt10s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt10s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt11s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt11s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt12s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt12s]]);
-
-	NSLog(@" ");
-
-	NSString *dt13s = @"2010-04-04T02:56:15Z";
-	NSString *dt14s = @"2010-04-04T21:56:15-05:00";
-	NSString *dt15s = @"2010-04-04T02:56:15.123Z";
-	NSString *dt16s = @"2010-04-04T21:56:15.123-05:00";
-
-	NSLog(@"S parseDateTime(%@) = %@", dt13s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt13s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt14s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt14s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt15s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt15s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt16s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt16s]]);
-
-	NSLog(@" ");
-
-	NSString *dt17s = @"2010-12-25T02:56:15Z";
-	NSString *dt18s = @"2010-12-25T21:56:15-05:00";
-	NSString *dt19s = @"2010-12-25T02:56:15.123Z";
-	NSString *dt20s = @"2010-12-25T21:56:15.123-05:00";
-
-	NSLog(@"S parseDateTime(%@) = %@", dt17s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt17s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt18s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt18s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt19s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt19s]]);
-	NSLog(@"S parseDateTime(%@) = %@", dt20s, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt20s]]);
-
-	NSLog(@" ");
-
-	// Should Fail
-
-	NSString *dt1f = nil;
-	NSString *dt2f = @"1969-07-20 21:56:15Z";
-	NSString *dt3f = @"1969-7-4T02:56:15.123Z";
-	NSString *dt4f = @"cheese";
-
-	NSLog(@"F parseDateTime(%@) = %@", dt1f, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt1f]]);
-	NSLog(@"F parseDateTime(%@) = %@", dt2f, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt2f]]);
-	NSLog(@"F parseDateTime(%@) = %@", dt3f, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt3f]]);
-	NSLog(@"F parseDateTime(%@) = %@", dt4f, [df stringFromDate:[XMPPDateTimeProfiles parseDateTime:dt4f]]);
-}
-
-+ (void)testCategory
-{
-	NSDate *now = [NSDate date];
-
-	NSLog(@"now(%@).xmppDateString = %@", now, [now xmppDateString]);
-	NSLog(@"now(%@).xmppTimeString = %@", now, [now xmppTimeString]);
-	NSLog(@"now(%@).xmppDateTimeString = %@", now, [now xmppDateTimeString]);
-}
-
-*/
--- Xcode/Testing/XMPPFramework-Tests-Common/Extensions/XEP-0082/XMPPDateTimeProfilesTest.m
+++ Xcode/Testing/XMPPFramework-Tests-Common/Extensions/XEP-0082/XMPPDateTimeProfilesTest.m
@@ -26,20 +26,16 @@
     NSTimeZone *_originalTimeZone;
 }
 
-- (void)setUpClass
-{
-    _dateFormatter = [[NSDateFormatter alloc] init];
-    [_dateFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
-    [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS z"];
-    [_dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
-}
-
 - (void)setUp
 {
     // Since dates do not have a explicit time zone, they are parsed in the
     // current timezone. To make this test repeatable we must fix the time zone.
     _originalTimeZone = [NSTimeZone defaultTimeZone];
-    [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+    [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneWithName:@"US/Pacific"]];
+
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    [_dateFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [_dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss.SSS z"];
 }
 
 - (void)tearDown
@@ -51,19 +47,19 @@
 - (void)testParseDateShouldSucceed
 {
     #define XMPPAssertParseDateEqualStrings(date, expected, description, ...) \
-        GHAssertEqualStrings([_dateFormatter stringFromDate:[XMPPDateTimeProfiles parseDate:(date)]], expected, description, ##__VA_ARGS__);
+        GHAssertEqualStrings([_dateFormatter stringFromDate:[XMPPDateTimeProfiles parseDate:date]], expected, description, ##__VA_ARGS__);
 
-    XMPPAssertParseDateEqualStrings(@"1776-07-04", @"1776-07-04 00:00:00.000 GMT-00:14:44", @"Should be standard GMT+HHMM as this is before DST was invented");
-    XMPPAssertParseDateEqualStrings(@"1969-01-21", @"1969-01-21 00:00:00.000 GMT+01:00", @"Takes place during daylight saving time");
-    XMPPAssertParseDateEqualStrings(@"1969-07-21", @"1969-07-21 00:00:00.000 GMT+01:00", @"Takes place during standard time.");
-	XMPPAssertParseDateEqualStrings(@"2010-04-04", @"2010-04-04 00:00:00.000 GMT+02:00", nil);
-	XMPPAssertParseDateEqualStrings(@"2010-12-25", @"2010-12-25 00:00:00.000 GMT+01:00", nil);
+    XMPPAssertParseDateEqualStrings(@"1776-07-04", @"1776-07-04 00:00:00.000 GMT-07:52:58", @"Should be standard GMT+HHMM as this is before DST was invented");
+    XMPPAssertParseDateEqualStrings(@"1969-01-21", @"1969-01-21 00:00:00.000 GMT-08:00", @"Takes place during daylight saving time");
+    XMPPAssertParseDateEqualStrings(@"1969-07-21", @"1969-07-21 00:00:00.000 GMT-07:00", @"Takes place during standard time.");
+	XMPPAssertParseDateEqualStrings(@"2010-04-04", @"2010-04-04 00:00:00.000 GMT-07:00", nil);
+	XMPPAssertParseDateEqualStrings(@"2010-12-25", @"2010-12-25 00:00:00.000 GMT-08:00", nil);
 }
 
 - (void)testParseDateShouldFail
 {
     #define XMPPAssertParseDateNil(date, description, ...) \
-        GHAssertNil([XMPPDateTimeProfiles parseDate:(date)], description, ##__VA_ARGS__);
+        GHAssertNil([XMPPDateTimeProfiles parseDate:date], description, ##__VA_ARGS__);
 
     XMPPAssertParseDateNil(nil, nil);
     XMPPAssertParseDateNil(@"1776-7-4", nil);
@@ -72,22 +68,86 @@
 
 - (void)testParseTimeShouldSucceed
 {
+    // The date part will be the current day
+    NSDateFormatter *dayFormatter = [[NSDateFormatter alloc] init];
+    [dayFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [dayFormatter setDateFormat:@"yyyy-MM-dd"];
 
+    // The tz part will be the one of the current day (DST/Standard)
+    NSDateFormatter *tzFormatter = [[NSDateFormatter alloc] init];
+    [tzFormatter setFormatterBehavior:NSDateFormatterBehavior10_4];
+    [tzFormatter setDateFormat:@"z"];
+
+    #define XMPPAssertParseTimeEqualStrings(time, expected, description, ...) \
+        do { \
+            NSString *_expected = [NSString stringWithFormat:@"%@ %@ %@", \
+                                   [dayFormatter stringFromDate:[NSDate date]], \
+                                   expected, \
+                                   [tzFormatter stringFromDate:[NSDate date]]]; \
+            GHAssertEqualStrings([_dateFormatter stringFromDate:[XMPPDateTimeProfiles parseTime:time]], _expected, description, ##__VA_ARGS__); \
+        } while (0)
+
+	XMPPAssertParseTimeEqualStrings(@"16:00:00",           @"16:00:00.000", nil);
+	XMPPAssertParseTimeEqualStrings(@"16:00:00Z",          @"08:00:00.000", nil);
+	XMPPAssertParseTimeEqualStrings(@"16:00:00-06:00",     @"14:00:00.000", nil);
+	XMPPAssertParseTimeEqualStrings(@"16:00:00.123",       @"16:00:00.123", nil);
+	XMPPAssertParseTimeEqualStrings(@"16:00:00.123Z",      @"08:00:00.123", nil);
+	XMPPAssertParseTimeEqualStrings(@"16:00:00.123-06:00", @"14:00:00.123", nil);
 }
 
 - (void)testParseTimeShouldFail
 {
+    #define XMPPAssertParseTimeNil(time, description, ...) \
+        GHAssertNil([XMPPDateTimeProfiles parseTime:(time)], description, ##__VA_ARGS__);
 
+	XMPPAssertParseTimeNil(nil, nil);
+	XMPPAssertParseTimeNil(@"16-00-00", nil);
+	XMPPAssertParseTimeNil(@"16:00:00-0600", nil);
+	XMPPAssertParseTimeNil(@"16:00:00.1", nil);
+	XMPPAssertParseTimeNil(@"16:00.123", nil);
+	XMPPAssertParseTimeNil(@"cheese", nil);
 }
 
 - (void)testParseDateTimeShouldSucceed
 {
+    #define XMPPAssertParseDateTimeEqualStrings(date, expected, description, ...) \
+        GHAssertEqualStrings([_dateFormatter stringFromDate:[XMPPDateTimeProfiles parseDateTime:date]], expected, description, ##__VA_ARGS__);
 
+	XMPPAssertParseDateTimeEqualStrings(@"1776-07-04T02:56:15Z",          @"1776-07-03 19:03:17.000 GMT-07:52:58", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1776-07-04T21:56:15-05:00",     @"1776-07-04 19:03:17.000 GMT-07:52:58", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1776-07-04T02:56:15.123Z",      @"1776-07-03 19:03:17.123 GMT-07:52:58", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1776-07-04T21:56:15.123-05:00", @"1776-07-04 19:03:17.123 GMT-07:52:58", nil);
+
+    XMPPAssertParseDateTimeEqualStrings(@"1969-01-21T02:56:15Z",          @"1969-01-20 18:56:15.000 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-01-20T21:56:15-05:00",     @"1969-01-20 18:56:15.000 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-01-21T02:56:15.123Z",      @"1969-01-20 18:56:15.123 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-01-20T21:56:15.123-05:00", @"1969-01-20 18:56:15.123 GMT-08:00", nil);
+
+    XMPPAssertParseDateTimeEqualStrings(@"1969-07-21T02:56:15Z",          @"1969-07-20 19:56:15.000 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-07-20T21:56:15-05:00",     @"1969-07-20 19:56:15.000 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-07-21T02:56:15.123Z",      @"1969-07-20 19:56:15.123 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"1969-07-20T21:56:15.123-05:00", @"1969-07-20 19:56:15.123 GMT-07:00", nil);
+
+    XMPPAssertParseDateTimeEqualStrings(@"2010-04-04T02:56:15Z",          @"2010-04-03 19:56:15.000 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-04-04T21:56:15-05:00",     @"2010-04-04 19:56:15.000 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-04-04T02:56:15.123Z",      @"2010-04-03 19:56:15.123 GMT-07:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-04-04T21:56:15.123-05:00", @"2010-04-04 19:56:15.123 GMT-07:00", nil);
+
+    XMPPAssertParseDateTimeEqualStrings(@"2010-12-25T02:56:15Z",          @"2010-12-24 18:56:15.000 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-12-25T21:56:15-05:00",     @"2010-12-25 18:56:15.000 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-12-25T02:56:15.123Z",      @"2010-12-24 18:56:15.123 GMT-08:00", nil);
+	XMPPAssertParseDateTimeEqualStrings(@"2010-12-25T21:56:15.123-05:00", @"2010-12-25 18:56:15.123 GMT-08:00", nil);
 }
 
 - (void)testParseDateTimeShouldFail
 {
-    
+    #define XMPPAssertParseDateTimeNil(date, description, ...) \
+        GHAssertNil([XMPPDateTimeProfiles parseDateTime:date], description, ##__VA_ARGS__);
+
+    XMPPAssertParseDateTimeNil(nil, nil);
+	XMPPAssertParseDateTimeNil(@"1969-07-20 21:56:15Z", nil);
+	XMPPAssertParseDateTimeNil(@"1969-7-4T02:56:15.123Z", nil);
+	XMPPAssertParseDateTimeNil(@"cheese", nil);
 }
 
 @end
